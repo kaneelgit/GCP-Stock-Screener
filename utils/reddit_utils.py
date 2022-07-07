f2d8016d5fb83ed57d185d43a8d4c92ee1df46d4
kaneelgit <kaneel.senevirathne@gmail.com>
2022-07-06 23:18:03 -0400

    added selftext

--- utils/reddit_utils.py
+++ utils/reddit_utils.py
@@ -1,3 +1,25 @@
 """
 Author - Kaneel Senevirathne
+Date - 07/05/2022
 """
+
+from typing_extensions import Self
+import praw
+import pandas as pd
+
+class reddit_data:
+
+    def __init__(self, client_id, secret, user_id):
+
+        self.client_id = client_id
+        self.secret = secret
+        self.user_id = user_id
+
+        #instance
+        self.reddit = praw.Reddit(
+            lient_id = self.client_id,
+            client_secret = self.secret, 
+            user_agent = self.user_id
+            )
+        
+    
